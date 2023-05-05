cbe99c2244a8715f0a5745caf8c87d5031ec59c3
influ <26268158+othman316@users.noreply.github.com>
2023-05-05 11:21:42 +0300

    next Image tag, style up

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,23 @@
+
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+    return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
+}
+
+export const postsRouter = createTRPCRouter({
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const posts = await ctx.prisma.post.findMany({ take: 100 });
+        const users = (await clerkClient.users.getUserList({
+            userId: posts.map(post => post.authorId), limit: 100
+        })).map(user => filterUserForClient(user))
+        return posts.map(post => {
+            const author = users.find(user => user.id === post.authorId)
+            if (!author || !author.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: 'Post author not found' })
+            return { post, author: { ...author, username: author.username } }
+        })
+    }),
+});
