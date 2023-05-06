0ed21b51e7a0e8220d7720ad8246f65e47922414
influ <26268158+othman316@users.noreply.github.com>
2023-05-06 12:43:51 +0300

    making post text-size bigger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ const PostView = (props: postWithAuthor) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
